fa5408da6b8af09f616a37a0058857072a12a9cc
Nick Adams <nadams@node-3.net>
2014-05-11 06:24:18 -0600

    Refactoring repo and services a bit to use a different kind of cake pattern

---
+++ app/components/ComponentRegistry.scala
@@ -0,0 +1,4 @@
+package components
+
+import data._
+import services._
---
+++ app/services/ProfileService.scala
@@ -0,0 +1,21 @@
+package services
+
+import data._
+
+trait ProfileServiceComponent {
+	val profileService: ProfileService
+
+	trait ProfileService {
+		def getByUsername(username: String) : Option[Profile]
+	}
+}
+
+trait ProfileServiceComponentImpl extends ProfileServiceComponent {
+	self: ProfileRepositoryComponent =>
+	override val profileService: ProfileService = new ProfileServiceImpl
+
+	private class ProfileServiceImpl extends ProfileService {
+		override def getByUsername(username: String) : Option[Profile] =
+			profileService.getByUsername(username)
+	}
+}
