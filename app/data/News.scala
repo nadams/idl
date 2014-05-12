b8c68dd33f6ed45289fd46f5f2adc6781579cb6b
Nick Adams <nadams@node-3.net>
2014-05-11 18:58:44 -0600

    Creating parsers for News class

--- app/data/News.scala
+++ app/data/News.scala
@@ -2,4 +2,9 @@ package data
 
 import com.github.nscala_time.time.Imports._
 
-case class News(newsId: Int, subject: String, dateCreated: DateTime, dateModified: DateTime, postedByProfile: Profile)
+case class News(newsId: Int, subject: String, dateCreated: DateTime, dateModified: DateTime, content: String, postedByProfileId: Int)
+
+object News {
+  def apply(x: (Int, String, DateTime, DateTime, String, Int)) : News = 
+    News(x._1, x._2, x._3, x._4, x._5, x._6)
+}
--- app/data/NewsRepository.scala
+++ app/data/NewsRepository.scala
@@ -8,12 +8,46 @@ trait NewsRepositoryComponent {
   }
 }
 
+trait NewsSchema {
+  val tableName = "News"
+  val newsId = "newsId"
+  val subject = "subject"
+  val dateCreated = "dateCreated"
+  val dateModified = "dateModified"
+  val content = "content"
+  val postedByProfileId = "postedByProfileId"
+}
+
 trait NewsRepositoryComponentImpl extends NewsRepositoryComponent {
   val newsRepository: NewsRepository = new NewsRepositoryImpl
 
-  class NewsRepositoryImpl extends NewsRepository {
-    override def getAllNews() : Seq[News] = {
-      Seq.empty[News]
+  class NewsRepositoryImpl extends NewsRepository with NewsSchema {
+    import java.sql._
+    import anorm._ 
+    import anorm.SqlParser._
+    import org.joda.time.DateTime
+    import play.api.db.DB
+    import play.api.Play.current
+    import AnormExtensions._
+
+    val newsParser = int(newsId) ~ str(subject) ~ get[DateTime](dateCreated) ~ get[DateTime](dateModified) ~ str(content) ~ int(postedByProfileId) map(flatten)
+    val multiRowNewsParser = newsParser *
+
+    override def getAllNews() : Seq[News] = DB.withConnection { implicit connection => 
+      SQL(
+        f"""
+          SELECT 
+            $newsId,
+            $subject,
+            $dateCreated,
+            $dateModified,
+            $content,
+            $postedByProfileId
+          FROM $tableName
+        """ 
+      )
+      .as(multiRowNewsParser)
+      .map(News(_))
     }
   }
 }
