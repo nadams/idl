ffba53d133e2c2355a4a1fb2405f5293726fc65a
Nick Adams <nadams@node-3.net>
2014-05-11 19:00:37 -0600

    Moving creation of Profile from a tuple into Profile class

--- app/data/ProfileRepository.scala
+++ app/data/ProfileRepository.scala
@@ -71,6 +71,6 @@ trait ProfileRepositoryComponentImpl extends ProfileRepositoryComponent {
 
     private def mapProfile(query: SimpleSql[Row])(implicit connection: Connection) : Option[Profile] = 
       query.singleOpt(int(profileId) ~ str(email) ~ str(displayName) ~ str(password) ~ bool(passwordExpired) ~ get[DateTime](dateCreated) ~ get[DateTime](lastLoginDate) map(flatten))
-      .map(x => Profile(x._1, x._2, x._3, x._4, x._5, x._6, x._7))
+      .map(Profile(_))
   }
 }
