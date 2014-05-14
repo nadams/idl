e5b07a501d7f250a704be308f4baac56c4711959
Nick Adams <nadams@node-3.net>
2014-05-14 07:24:17 -0600

    Adding action links to create or edit depending on if the news id is 0 or not

--- app/models/news/EditNews.scala
+++ app/models/news/EditNews.scala
@@ -1,6 +1,8 @@
 package models.news
 
-case class EditNews(newsId: Int)
+case class EditNews(newsId: Int) {
+  val isNewNews = newsId == 0
+}
 
 object EditNews {
   def apply() : EditNews = EditNews(0)
