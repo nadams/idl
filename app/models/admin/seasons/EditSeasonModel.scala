6fabaab1c57e9ad005b4a8d344272a43e3fe40e6
Nick Adams <nadams@node-3.net>
2014-05-24 15:46:35 -0600

    Implementing a basic parser for DateTime

--- app/models/admin/seasons/EditSeasonModel.scala
+++ app/models/admin/seasons/EditSeasonModel.scala
@@ -2,16 +2,14 @@ package models.admin.seasons
 
 import play.api.libs.json._
 import org.joda.time.{ DateTime, DateTimeZone }
-import org.joda.time.format.{ ISODateTimeFormat }
 
 case class EditSeasonModel(seasonId: Int, name: String, startDate: DateTime, endDate: DateTime) {
   val isNewSeason = seasonId == 0
 }
 
 object EditSeasonModel {
-  implicit val writesDateTime: Writes[DateTime] = new Writes[DateTime] {
-    def writes(d: DateTime) = JsString(ISODateTimeFormat.dateTime().withZone(DateTimeZone.UTC).print(d));
-  }
+  import formatters.DateTimeFormatter
+
   implicit val writes = Json.writes[EditSeasonModel]
 
   lazy val now = new DateTime(DateTimeZone.UTC)
