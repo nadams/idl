9c12e25409aa555054be6d2f9968b9196e25662d
Nick Adams <nadams@node-3.net>
2014-05-25 10:31:20 -0600

    Fetching seasons and sending to view

--- app/controllers/TeamController.scala
+++ app/controllers/TeamController.scala
@@ -2,10 +2,17 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import components._
 import models.admin.teams._
 
-object TeamController extends Controller with ProvidesHeader with Secured {
+object TeamController extends Controller with ProvidesHeader with Secured with SeasonComponentImpl {
   def index = IsAuthenticated { username => implicit request =>
-    Ok(views.html.admin.teams.index(TeamIndexModel.empty))
+    Ok(views.html.admin.teams.index(
+      TeamIndexModel(
+        seasonService.getAllSeasons.map { season => 
+          SeasonModel(season.seasonId, season.name)
+        }
+      )
+    ))
   }
 }
