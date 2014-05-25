e9f158ab7ae9d4e65d8ac2107fe75e0ccec103ac
Nick Adams <nadams@node-3.net>
2014-05-25 10:21:54 -0600

    Creating basic javascript

--- public/javascripts/views/admin/teams/Teams.js
+++ public/javascripts/views/admin/teams/Teams.js
@@ -7,15 +7,43 @@ admin.teams.index = admin.teams.index || {};
 
 admin.teams.index.IndexModel = (function(ko, _) {
 	var Model = function(data) {
+		this.seasons = [];
+		this.selectedSeason = ko.observable();
 
 		this.initialize(data);
 	};
 
-	ko.utils.extend(Model, {
+	ko.utils.extend(Model.prototype, {
 		initialize: function(data) {
-
+			this.seasons = _.map(data, function(seasonData) {
+				return new admin.teams.index.SeasonModel(seasonData);
+			}, this);
 		}
 	});
 
 	return Model;
 })(ko, _);
+
+admin.teams.index.SeasonModel = (function(ko) {
+	var Model = function(data) {
+		this.seasonId = 0;
+		this.seasonName = '';
+
+		this.initialize(data);
+	};
+
+	ko.utils.extend(Model.prototype, {
+		initialize: function(data) {
+			this.seasonId = data.seasonId;
+			this.seasonName = data.seasonName;
+		}
+	});
+
+	return Model;
+})(ko);
+
+(function(ko) {
+	var model = new admin.teams.index.IndexModel(admin.teams.index.data);
+
+	ko.applyBindings(model);
+})(ko);
