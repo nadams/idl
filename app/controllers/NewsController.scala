5626b6fcc50e6e5efd585d3ea14d479a197e1d91
Nick Adams <nadams@node-3.net>
2014-05-29 20:36:30 -0600

    Implementing the role based IsAuthenticated on the news controller

--- app/controllers/NewsController.scala
+++ app/controllers/NewsController.scala
@@ -2,13 +2,13 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import org.joda.time.{ DateTime, DateTimeZone }
 import components._
 import _root_.data.News
 import models.admin.news._
 import models.FieldExtensions._
 import models.FormExtensions._
 import security.Roles
-import org.joda.time.{ DateTime, DateTimeZone }
 
 object NewsController extends Controller with ProvidesHeader with Secured with NewsComponentImpl with ProfileComponentImpl {
   val couldNotUpdateNewsItem = InternalServerError("Could not create news item")
@@ -17,35 +17,35 @@ object NewsController extends Controller with ProvidesHeader with Secured with N
     Ok(views.html.admin.news.index(AdminNews.toModels(newsService.getAllNews, username, routes.NewsController)))
   }
 
-  def create = IsAuthenticated { username => implicit request =>
+  def create = IsAuthenticated(Roles.Admin) { username => implicit request =>
     Ok(views.html.admin.news.edit(EditNews.empty, EditNewsErrors()))
   }
 
-  def saveNew = IsAuthenticated { username => implicit request =>
+  def saveNew = IsAuthenticated(Roles.Admin) { username => implicit request =>
     val now = new DateTime(DateTimeZone.UTC)
     
     updateNews((model, profileId) => newsService.insertNews(News(model.newsId, model.subject, now, now, model.content, profileId)), username)
   }
 
-  def saveExisting(id: Int) = IsAuthenticated { username => implicit request =>
+  def saveExisting(id: Int) = IsAuthenticated(Roles.Admin) { username => implicit request =>
     updateNews((news, profileId) => newsService.updateNews(news.newsId, news.subject, news.content), username)
   }
 
-  def edit(id: Int) = IsAuthenticated { username => implicit request =>
+  def edit(id: Int) = IsAuthenticated(Roles.Admin) { username => implicit request =>
     newsService.getNewsById(id) match {
       case Some(x) => Ok(views.html.admin.news.edit(EditNews.toModel(x), EditNewsErrors()))
       case None => Redirect(routes.NewsController.create)
     }
   }
 
-  def remove(id: Int) = IsAuthenticated { username => implicit request =>
+  def remove(id: Int) = IsAuthenticated(Roles.Admin) { username => implicit request =>
     newsService.removeNewsItem(id) match {
       case true => Redirect(routes.NewsController.index)
       case false => InternalServerError("Could not remove news item")
     }
   }
 
-  def updateNews(saveAction: (EditNews, Int) => Boolean, username: String)(implicit request: Request[AnyContent]) : Result = 
+  private def updateNews(saveAction: (EditNews, Int) => Boolean, username: String)(implicit request: Request[AnyContent]) : Result = 
     EditNewsForm().bindFromRequest.fold(
       content => {
         val subjectError = content("subject").formattedMessage
