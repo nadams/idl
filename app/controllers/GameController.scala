8df233a013f4c450e8a3534a467fc0f6dc006a1d
Nick Adams <nick.adams@mediaforge.com>
2014-07-07 17:04:07 -0600

    Adding helper method to only allow actions that result in an actual season value. Otherwise, show a 404

--- app/controllers/GameController.scala
+++ app/controllers/GameController.scala
@@ -4,10 +4,18 @@ import play.api._
 import play.api.mvc._
 import play.api.data.Forms._
 import play.api.data._
+import components._
+import _root_.data._
 import security.Roles
 
-object GameController extends Controller with ProvidesHeader with Secured {
-  def index(id: Int) = IsAuthenticated(Roles.Admin) { username => implicit request => 
+object GameController extends Controller with ProvidesHeader with Secured with SeasonComponentImpl {
+  def index(id: Int) = HasSeason(id) { season => 
     Ok("")
+  }
+
+  def HasSeason(seasonId: Int)(f: Season => Result) = IsAuthenticated(Roles.Admin) { username => implicit request => 
+    seasonService.getSeasonById(seasonId)
+      .map(f(_))
+      .getOrElse(NotFound(s"Season $seasonId not found"))
   }
 }
