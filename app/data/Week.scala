67abadf3a3e8f03f06ca3ff8bebd9f62fd25cf2e
Nick Adams <nadams@node-3.net>
2014-07-14 16:55:00 -0600

    Creating Weeks enum

---
+++ app/data/Week.scala
@@ -0,0 +1,16 @@
+package data
+
+object Weeks extends Enumeration {
+  type Week = Value
+
+  val Week1 = Value(1, "Week 1")
+  val Week2 = Value(1, "Week 2")
+  val Week3 = Value(1, "Week 3")
+  val Week4 = Value(1, "Week 4")
+  val Week5 = Value(1, "Week 5")
+  val Week6 = Value(1, "Week 6")
+  val Week7 = Value(1, "Week 7")
+  val Week8 = Value(1, "Week 8")
+  val Week9 = Value(1, "Week 9")
+  val Week10 = Value(1, "Week 10")
+}
