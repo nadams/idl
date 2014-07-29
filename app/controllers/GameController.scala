88c1a468127b782eebb04bfef77c4340d6fca0a2
Nick Adams <nick.adams@mediaforge.com>
2014-07-29 13:45:34 -0600

    Passing the seasonId to the view for link generation

--- app/controllers/GameController.scala
+++ app/controllers/GameController.scala
@@ -3,7 +3,6 @@ package controllers
 import play.api._
 import play.api.mvc._
 import play.api.libs.json._
-import org.joda.time.{ DateTime, DateTimeZone }
 import components._
 import _root_.data._
 import models.admin.games._
@@ -12,11 +11,21 @@ import models.FormExtensions._
 import extensions.DateTimeExtensions._
 import security.Roles
 
-object GameController extends Controller with Secured with ProvidesHeader with TeamComponentImpl with SeasonComponentImpl with GameComponentImpl {
-  def index(seasonId: Int) = HasSeason(seasonId) { username => implicit request => 
+object GameController extends Controller 
+  with Secured 
+  with ProvidesHeader 
+  with TeamComponentImpl 
+  with SeasonComponentImpl 
+  with GameComponentImpl {
+  
+  def index(implicit seasonId: Int) = HasSeason(seasonId) { username => implicit request => 
     val data = gameService.getGamesBySeasonId(seasonId) map(game => (game, teamService.getTeamsForGame(game.gameId)))
 
     Ok(views.html.admin.games.index(GamesModel.toModel(data)))
+  }
+
+  def create(implicit seasonId: Int) = HasSeason(seasonId) { username => implicit request => 
+    Ok("")
   }
 
   private def HasSeason(seasonId: Int)(f: => Season => Request[AnyContent] => Result) = IsAuthenticated(Roles.Admin) { username => implicit request => 
