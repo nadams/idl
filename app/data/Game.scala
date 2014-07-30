076c99eb3723bb7f4c7b4e2f5251847186d77703
Nick Adams <nick.adams@mediaforge.com>
2014-07-30 15:37:47 -0600

    Computing the status of the game based on the datecompleted value

--- app/data/Game.scala
+++ app/data/Game.scala
@@ -8,11 +8,19 @@ case class Game(
   seasonId: Int, 
   scheduledPlayTime: DateTime, 
   dateCompleted: Option[DateTime],
-  teams: Option[(Int, Int)]
-)
+  teams: Option[(Int, Int)]) {
+
+  lazy val status = dateCompleted.map(date => GameStatus.Completed).getOrElse(GameStatus.Pending)
+}
 
 object Game {
   def apply(data: (Int, Int, Int, DateTime, Option[DateTime], Option[Int], Option[Int])) : Game = 
     if(data._6.isDefined && data._7.isDefined) Game(data._1, data._2, data._3, data._4, data._5, Some(data._6.get, data._7.get))
     else Game(data._1, data._2, data._3, data._4, data._5, None)
+}
+
+object GameStatus extends Enumeration {
+  type Status = Value
+
+  val Pending, Completed = Value
 }
