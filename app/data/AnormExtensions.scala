ad6ffc93a52968717f2d5b8f8aba164ab50c6add
nadams <nadams@node-3.net>
2014-08-05 16:42:16 -0600

    Creating a binary data row parser. Creating an alias to get a datetime value

--- app/data/AnormExtensions.scala
+++ app/data/AnormExtensions.scala
@@ -2,6 +2,7 @@ package data
 
 import java.sql.Timestamp
 import anorm._
+import anorm.SqlParser._
 import org.joda.time._
 import org.joda.time.format._
 
@@ -33,4 +34,15 @@ object AnormExtensions {
     def set(s: PreparedStatement, i: Int, data: Array[Byte]) : Unit = 
       s.setBinaryStream(i, new ByteArrayInputStream(data), data.length)
   }
+
+  implicit def rowToByteArray : Column[Array[Byte]] = Column.nonNull { (value, meta) => 
+    val MetaDataItem(qualified, nullable, clazz) = meta
+    value match {
+      case data: Array[Byte] => Right(data)
+      case _ => Left(TypeDoesNotMatch(s"Cannot convert $value:${value.asInstanceOf[AnyRef].getClass} to byte array for column $qualified"))
+    }
+  }
+
+  def bytes(columnName: String) = get[Array[Byte]](columnName)(implicitly[Column[Array[Byte]]])
+  def datetime(columnName: String) = get[DateTime](columnName)(implicitly[Column[DateTime]])
 }
