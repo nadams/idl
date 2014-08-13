8a4bafc68af4919181a6171b193f36b8f13b0cff
Nick Adams <nick.adams@mediaforge.com>
2014-08-13 15:56:02 -0600

    Creating model for regular season stats

--- app/models/brackets/IndexModel.scala
+++ app/models/brackets/IndexModel.scala
@@ -3,17 +3,26 @@ package models.brackets
 import play.api.libs.json.Json
 import data._
 
-case class IndexModel(playoffStats: Seq[PlayoffStatsModel])
+case class IndexModel(playoffStats: Seq[PlayoffStatsModel], regularStats: Seq[RegularSeasonStatsModel])
 case class PlayoffStatsModel(teamId: Int, teamName: String, gameId: Int, weekId: Int, captures: Int)
+case class RegularSeasonStatsModel(teamId: Int, teamName: String, gameId: Int, wins: Int, losses: Int, ties: Int)
 
 object IndexModel {
+  implicit val writesRegularSeasonStatsModel = Json.writes[RegularSeasonStatsModel]
   implicit val writesPlayoffStatsModel = Json.writes[PlayoffStatsModel]
   implicit val writesIndexModel = Json.writes[IndexModel]
 
-  def toModel(playoffStats: Seq[TeamGameResultRecord]) = IndexModel(playoffStats.map(PlayoffStatsModel.toModel(_)))
+  def toModel(playoffStats: Seq[TeamGameResultRecord]) = IndexModel(
+    playoffStats.map(PlayoffStatsModel.toModel(_)),
+    Seq.empty[RegularSeasonStatsModel]
+  )
 }
 
 object PlayoffStatsModel {
   def toModel(data: TeamGameResultRecord) = 
     PlayoffStatsModel(data.teamId, data.teamName, data.gameId, data.weekId, data.captures)
+}
+
+object RegularSeasonStatsModel {
+  def toModel() = ???
 }
