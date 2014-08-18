f9e6637e9b2fa66c349b42ece9ef8057eaf2af11
Nick Adams <nadams@node-3.net>
2014-08-18 13:52:17 -0600

    Initial work

--- app/models/brackets/IndexModel.scala
+++ app/models/brackets/IndexModel.scala
@@ -12,9 +12,9 @@ object IndexModel {
   implicit val writesPlayoffStatsModel = Json.writes[PlayoffStatsModel]
   implicit val writesIndexModel = Json.writes[IndexModel]
 
-  def toModel(playoffStats: Seq[TeamGameResultRecord]) = IndexModel(
-    playoffStats.map(PlayoffStatsModel.toModel(_)),
-    Seq.empty[RegularSeasonStatsModel]
+  def toModel(stats: Seq[TeamGameResultRecord]) = IndexModel(
+    stats.map(PlayoffStatsModel.toModel(_)),
+    RegularSeasonStatsModel.toModel(stats)
   )
 }
 
@@ -24,5 +24,35 @@ object PlayoffStatsModel {
 }
 
 object RegularSeasonStatsModel {
-  def toModel() = ???
+  class TempStats(val teamId: Int, val teamName: String, val gameId: Int, var wins: Int, var losses: Int, var ties: Int)
+
+  def toModel(stats: Seq[TeamGameResultRecord]) : Seq[RegularSeasonStatsModel] = {
+    val winners = stats.groupBy(_.gameId).mapValues { x =>
+      val max = x.maxBy(_.captures).captures
+      x.filter(y => y.captures == max)
+    }.filter(_._2.size == 1)
+
+    val losers = stats.groupBy(_.gameId).mapValues { x =>
+      val min = x.minBy(_.captures).captures
+      x.filter(y => y.captures == min)
+    }.filter(_._2.size == 1)
+    
+    val ties = stats.groupBy(_.gameId)
+      .mapValues(_.groupBy(_.captures).values.filter(_.size > 1).flatMap(x => x).toList)
+      .filter(_._2.size > 1)
+
+    val models = scala.collection.mutable.List[RegularSeasonStatsModel]();
+    modes ++ winners.values.groupBy(_.teamId).map(_._2.size)
+
+
+
+    models
+  }
+
+  def join[A, B, K, R](outer: Traversable[A], inner: Traversable[B])(outKey: A => K, inKey: B => K, f: (A, B) => R): Traversable[R] = {
+    for(o <- outer; i <- inner; if outKey(o) == inKey(i)) yield f(o, i)
+  }
 }
+
+
+
