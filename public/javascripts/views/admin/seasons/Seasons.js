bfaa14623046a86f3f560e0165414b6ad1e3a6af
Nick <nadams@node-3.net>
2014-09-18 11:18:49 -0600

    Adding initial sorting to the seasons index page. Formatting JS file.

--- public/javascripts/views/admin/seasons/Seasons.js
+++ public/javascripts/views/admin/seasons/Seasons.js
@@ -6,105 +6,125 @@ admin.seasons = admin.seasons || {};
 admin.seasons.index = admin.seasons.index || {};
 
 admin.seasons.index.SeasonModel = (function(ko, moment) {
-	var Model = function(data, repository) {
-		this.repository = repository;
+  var Model = function(data, repository) {
+    this.repository = repository;
 
-		this.seasonId = 0;
-		this.name = '';
-		this.startDate = ko.observable();
-		this.endDate = ko.observable();
-		this.editLink = '';
-		this.removeLink = '';
-		this.manageTeamsLink = '';
-		this.manageGamesLink = '';
-		this.confirmationShown = ko.observable(false);
-		this.isRemovingSeason = ko.observable(false);
+    this.seasonId = 0;
+    this.name = '';
+    this.startDate = ko.observable();
+    this.endDate = ko.observable();
+    this.editLink = '';
+    this.removeLink = '';
+    this.manageTeamsLink = '';
+    this.manageGamesLink = '';
+    this.confirmationShown = ko.observable(false);
+    this.isRemovingSeason = ko.observable(false);
 
-		this.initialize(data);
+    this.initialize(data);
 
-		this.niceStartDate = ko.computed(function() {
-			return this.niceDate(this.startDate());
-		}, this);
+    this.niceStartDate = ko.computed(function() {
+      return this.niceDate(this.startDate());
+    }, this);
 
-		this.niceEndDate = ko.computed(function() {
-			return this.niceDate(this.endDate());
-		}, this);
-	};
+    this.niceEndDate = ko.computed(function() {
+      return this.niceDate(this.endDate());
+    }, this);
+  };
 
-	ko.utils.extend(Model.prototype, {
-		initialize: function(data) {
-			this.seasonId = data.seasonId;
-			this.name = data.name;
-			this.startDate(data.startDate);
-			this.endDate(data.endDate);
-			this.editLink = data.editLink;
-			this.removeLink = data.removeLink;
-			this.manageTeamsLink = data.manageTeamsLink;
-			this.manageGamesLink = data.manageGamesLink;
-		},
-		niceDate: function(date) {
-			return moment.utc(date).local().format('LLL');
-		},
-		showRemoveConfirmation: function() {
-			this.confirmationShown(true);
-		},
-		hideRemoveConfirmation: function() {
-			this.confirmationShown(false);
-		},
-		removeSeason: function(parent) {
-			this.isRemovingSeason(true);
-			var promise = this.repository.removeSeason(this.seasonId, this);
+  ko.utils.extend(Model.prototype, {
+    initialize: function(data) {
+      this.seasonId = data.seasonId;
+      this.name = data.name;
+      this.startDate(data.startDate);
+      this.endDate(data.endDate);
+      this.editLink = data.editLink;
+      this.removeLink = data.removeLink;
+      this.manageTeamsLink = data.manageTeamsLink;
+      this.manageGamesLink = data.manageGamesLink;
+    },
+    niceDate: function(date) {
+      return moment.utc(date).local().format('LLL');
+    },
+    showRemoveConfirmation: function() {
+      this.confirmationShown(true);
+    },
+    hideRemoveConfirmation: function() {
+      this.confirmationShown(false);
+    },
+    removeSeason: function(parent) {
+      this.isRemovingSeason(true);
+      var promise = this.repository.removeSeason(this.seasonId, this);
 
-			promise.always(function() {
-				this.isRemovingSeason(false);
-				this.confirmationShown(false);
-			});
+      promise.always(function() {
+        this.isRemovingSeason(false);
+        this.confirmationShown(false);
+      });
 
-			promise.done(function(data) {
-				var seasonToRemove = _.find(parent.seasons(), function(item) {
-					return item.seasonId === data;
-				});
+      promise.done(function(data) {
+        var seasonToRemove = _.find(parent.seasons(), function(item) {
+          return item.seasonId === data;
+        });
 
-				parent.seasons.remove(seasonToRemove);
-			});
+        parent.seasons.remove(seasonToRemove);
+      });
 
-			promise.fail(function(result) {
-				parent.errorMessage(result.responseText);
-			});
-		}
-	});
+      promise.fail(function(result) {
+        parent.errorMessage(result.responseText);
+      });
+    }
+  });
 
-	return Model;
+  return Model;
 })(ko, moment);
 
 admin.seasons.index.IndexModel = (function(ko, _) {
-	var Model = function(data, repository) {
-		this.seasons = ko.observableArray([]);
-		this.errorMessage = ko.observable('');
+  var Model = function(data, repository) {
+    this.seasons = ko.observableArray([]);
+    this.errorMessage = ko.observable('');
 
-		this.initialize(data, repository);
+    this.initialize(data, repository);
 
-		this.errorMessageShown = ko.computed(function() {
-			return this.errorMessage().length > 0;
-		}, this);
-	};
+    this.errorMessageShown = ko.computed(function() {
+      return this.errorMessage().length > 0;
+    }, this);
 
-	ko.utils.extend(Model.prototype, {
-		initialize: function(data, repository) {
-			this.seasons(_.map(data, function(item) {
-				return new admin.seasons.index.SeasonModel(item, repository);
-			}, this));
-		},
-		clearErrorMessage: function() {
-			this.errorMessage('');
-		}
-	});
+    this.sortedSeasons = ko.computed(function() {
+      return this.seasons.sort(function(left, right) {
+        if(left.startDate() < right.startDate()) {
+          if(left.endDate() < right.endDate()) {
+            return 1;
+          } else if (left.endDate() > right.endDate()) {
+            return -1;
+          }
+        } else if(left.startDate() > right.startDate()) {
+          if(left.endDate() > right.endDate()) {
+            return 1;
+          } else if (left.endDate() < right.endDate()) {
+            return -1;
+          }
+        }
 
-	return Model;
+        return 0;
+      });
+    }, this);
+  };
+
+  ko.utils.extend(Model.prototype, {
+    initialize: function(data, repository) {
+      this.seasons(_.map(data, function(item) {
+        return new admin.seasons.index.SeasonModel(item, repository);
+      }, this));
+    },
+    clearErrorMessage: function() {
+      this.errorMessage('');
+    }
+  });
+
+  return Model;
 })(ko, _);
 
 (function(ko) {
-	var model = new admin.seasons.index.IndexModel(admin.seasons.index.data, new admin.seasons.SeasonsRepository());
+  var model = new admin.seasons.index.IndexModel(admin.seasons.index.data, new admin.seasons.SeasonsRepository());
 
-	ko.applyBindings(model);
+  ko.applyBindings(model);
 })(ko);
