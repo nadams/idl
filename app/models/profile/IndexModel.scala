3d2b7c2d0d31399f12ab1006730139e0cb6a36c1
Nick Adams <nadams@node-3.net>
2014-10-20 06:43:48 -0600

    Using more knockout

--- app/models/profile/IndexModel.scala
+++ app/models/profile/IndexModel.scala
@@ -1,3 +1,9 @@
 package models.profile
 
+import play.api.libs.json.Json
+
 case class IndexModel(profileId: Int, profileIsPlayer: Boolean)
+
+object IndexModel {
+  implicit val writesIndexModel = Json.writes[IndexModel] 
+}
--- public/javascripts/views/profile/IndexModel.js
+++ public/javascripts/views/profile/IndexModel.js
@@ -1,22 +1,26 @@
-/* global ko */
+/* global ko, profile */
 
-(function(ko) {
+(function(ko, profile) {
   'use strict';
 
-  var profile = profile || {};
-  profile.index = profile.index || {};
   profile.index.IndexModel = (function() {
     var Model = function(data) {
-    
+      this.profileId = ko.observable();
+      this.profileIsPlayer = ko.observable(false);
+
       this.initialize(data);
     };
 
     ko.utils.extend(Model.prototype, {
       initialize: function(data) {
-      
+        this.profileId(data.profileId);
+        this.profileIsPlayer(data.profileIsPlayer);
       }
     });
 
     return Model;
   })();
-})(ko);
+
+  var model = new profile.index.IndexModel(profile.index.data);
+  ko.applyBindings(model);
+})(ko, profile);
