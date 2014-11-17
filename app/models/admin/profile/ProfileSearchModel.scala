711780dffc57aaf74f043760ab6754881ce41d70
Nick <nadams@node-3.net>
2014-11-17 16:13:22 -0700

    Creating model for player name search

---
+++ app/models/admin/profile/ProfileSearchModel.scala
@@ -0,0 +1,21 @@
+package models.admin.profile
+
+import data.ProfileSearchRecord
+
+case class ProfileSearchModel(results: Seq[ProfileSearchResultModel])
+
+object ProfileSearchModel {
+  def toModel(data: Seq[ProfileSearchRecord]) = 
+    data.groupBy(x => (x.profileId, x.displayName, x.email)).map { case (key, value) =>
+      ProfileSearchResultModel(key._1, key._2, key._3, value.map(PlayerNameSearchResultModel.toModel(_)))
+    }
+}
+
+case class ProfileSearchResultModel(profileId: Int, displayName: String, email: String, playerNames: Seq[PlayerNameSearchResultModel])
+
+case class PlayerNameSearchResultModel(playerId: Int, playerName: String, isApproved: Boolean)
+
+object PlayerNameSearchResultModel {
+  def toModel(data: ProfileSearchRecord) = 
+    PlayerNameSearchResultModel(data.playerId, data.playerName, data.isApproved)
+}
